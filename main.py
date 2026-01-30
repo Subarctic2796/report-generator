96341e55f0a64e9966bd4b7ae58feb9a9c7d87a3
Subarctic2796 <isuckatcoding74@gmail.com>
2026-01-30 13:52:06 +0200

    add pyqt5

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from collections import Counter
 from datetime import datetime, date
 from os.path import isfile
 from sys import stderr, argv, exit
 from typing import TypeAlias, Literal
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QPushButton, QMessageBox
 
 import pyexcel as pyxl
 
@@ -16,9 +18,12 @@ Row: TypeAlias = tuple[str, str | int, str | int]
 def get_info(path: str, key: Literal["assigned", "aps_rec"]) -> ExtractedInfo | None:
     """assumes that `path` exists.
     `key` can only be the string 'assigned' or 'aps_rec'"""
+    print(f"[INFO]: [get_info]: attempting to read '{path}'")
     records = pyxl.iget_records(file_name=path)
     cntr: ExtractedInfo = Counter()
-    for i in records:
+    for ix, i in enumerate(records):
+        if ix == 0:
+            print(i)
         try:
             name: str = i["retriever"]
         except KeyError:
@@ -30,7 +35,7 @@ def get_info(path: str, key: Literal["assigned", "aps_rec"]) -> ExtractedInfo | 
         date_: datetime | date = i[key].date() if isinstance(i[key], datetime) else i[key]
         cntr[(name, date_)] += 1
     pyxl.free_resources()
-    print(f"successfully read {'assigned' if key == 'assigned' else 'closed'} file: {path}")
+    print(f"[INFO]: [get_info]: successfully read {'assigned' if key == 'assigned' else 'closed'} file: {path}")
     return cntr
 
 
@@ -66,17 +71,74 @@ def generate_assigned_to_closed(assigned_path: str, closed_path: str, output_pat
     print(f"successfully created {output_path}")
     return True
 
+class MainWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("report generator")
+        self.resize(720, 480)
+        self.setAcceptDrops(True)
+
+        self.assign_path: str | None = None
+        self.close_path: str | None = None
+
+        self.get_assigned()
+        self.get_closed()
+        self.save_btn()
+
+    def get_assigned(self):
+        self.button = QPushButton("Choose assigned file", self)
+        self.button.clicked.connect(self.openFileDialog_assigned)
+        self.button.setGeometry(150, 150, 150, 30)
+
+    def get_closed(self):
+        self.button = QPushButton("Choose closed file", self)
+        self.button.clicked.connect(self.openFileDialog_closed)
+        self.button.setGeometry(150, 180, 150, 30)
+
+    def save_btn(self):
+        self.button = QPushButton("Create assigned to closed", self)
+        self.button.clicked.connect(self.createClosed)
+        self.button.setGeometry(150, 210, 150, 30)
+
+    def createClosed(self):
+        msg_box = QMessageBox()
+        can_save = self.close_path is not None and self.assign_path is not None
+        if can_save:
+            save_path = str(Path.home()) + f"\\Desktop\\output_{date.today()}.xlsx"
+            status = generate_assigned_to_closed(self.assign_path, self.close_path, save_path)
+            if status:
+                msg_box.setText("Successfully created output")
+            else:
+                msg_box.setText("Failed to create consolidated file, check logs")
+        else:
+            msg_box.setText("must choose a closed and assigned file")
+        msg_box.exec()
+
+    def openFileDialog_closed(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Choose a closed file")
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            self.close_path = selected_files[0]
+            print("Selected File:", selected_files[0])
+
+    def openFileDialog_assigned(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setWindowTitle("Choose an assigned file")
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+        file_dialog.setViewMode(QFileDialog.ViewMode.Detail)
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            self.assign_path = selected_files[0]
+            print("Selected File:", selected_files[0])
+
 
 if __name__ == '__main__':
-    if len(argv) != 4:
-        print("usage: report-gen /path/to/assigned.xls, /path/to/closed.xls /path/to/output.xlsx")
-        exit(1)
-
-    assigned, closed, output = argv[1:]
-    if not output.endswith(".xlsx"):
-        print("[ERROR]: [main]: output path must end with xlsx", file=stderr)
-        exit(1)
-
-    if not generate_assigned_to_closed(assigned, closed, output):
-        print("[ERROR]: [main]: unable to create consolidated output", file=stderr)
-        exit(1)
+    app = QApplication(argv)
+    ui = MainWidget()
+    ui.show()
+    exit(app.exec())
